c882377ec18e7a6407905c379702b24ddc1fdf4e
Darin Hunt <40468660+dhunt10@users.noreply.github.com>
2019-11-24 21:23:58 -0500

    Update CompositeSpreadsheetController.java

--- src/edu/cs3500/spreadsheets/controller/CompositeSpreadsheetController.java
+++ src/edu/cs3500/spreadsheets/controller/CompositeSpreadsheetController.java
@@ -1,17 +1,36 @@
 package edu.cs3500.spreadsheets.controller;
 
-import edu.cs3500.spreadsheets.BeyondGood;
-import edu.cs3500.spreadsheets.model.Spreadsheet;
+import edu.cs3500.spreadsheets.view.CompositeViewAction;
+import java.awt.event.MouseEvent;
 
-public class CompositeSpreadsheetController implements SpreadsheetController {
+public class CompositeSpreadsheetController implements SpreadsheetController, CompositeViewAction {
 
-  public CompositeSpreadsheetController() {
+  @Override
+  public void mouseClicked(MouseEvent e) {
+      //gotta somehow get these coordinates then convert them to cell Coords
+      int x = e.getX();
+      int y = e.getY();
+  }
+
+
+
+  @Override
+  public void mousePressed(MouseEvent e) {
 
   }
 
   @Override
-  public void updateProgram(String coordinate, String inString, Spreadsheet s) {
-    BeyondGood.updateCurrentView(coordinate, inString, s);
+  public void mouseReleased(MouseEvent e) {
+
   }
 
+  @Override
+  public void mouseEntered(MouseEvent e) {
+
+  }
+
+  @Override
+  public void mouseExited(MouseEvent e) {
+
+  }
 }
